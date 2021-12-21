3469ec697eac0d3c6ba9fca4a522f9f32f550dfa
Zhen Ju <juzhen@huawei.com>
2021-12-21 15:08:22 +0800

    Add github webhook secret validations. Add logging

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from fastapi import FastAPI, Request
+import os
 
+from fastapi import FastAPI, HTTPException, Request, status
+from loguru import logger
+
+from validators import validate_secret, validate_sender
+
+validators = [validate_secret, validate_sender]
 app = FastAPI()
 
+LOG_FILEPATH = os.environ.get('LOG_FILEPATH') or './webhook.log'
+print(LOG_FILEPATH)
+logger.add(LOG_FILEPATH,
+           format='[{level}]\t{name}:{function}:{line}\t{message}')
 
-@app.post("/payload")
-async def read_payload(req: Request):
-    req_obj = await req.json()
-    return {"Hello": "World"}
 
 @app.post("/")
 async def read_root_hook(req: Request):
-    req_obj = await req.json()
+    for validator in validators:
+        is_valid = await validator(req)
+        if not is_valid:
+            logger.error(f'Failed to validate request: {validator.__name__}')
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+            return
+    #  req_obj = await req.json()
+
     return {"Hello": "World"}
