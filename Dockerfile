4cbd752049420efbbbd32ea941b14b8003f4ad68
Zhen Ju <juzhen@huawei.com>
2021-12-22 17:01:39 +0800

    Clone the jobs git repo and copy the dags to dst folder.

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ RUN git clone https://github.com/crystaldust/airflow-jobs-hook.git /airflow-jobs
 
 FROM python:3.8-alpine
 COPY --from=0 /airflow-jobs-hook /airflow-jobs-hook
-WORKDIR /airflow-jobs-hook 
+WORKDIR /airflow-jobs-hook
 RUN pip install -r ./requirements.txt
 
 CMD ["uvicorn main:app"]
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
+import shutil
+from os import environ, path
 
 from fastapi import FastAPI, HTTPException, Request, status
+from git import Git, Repo
 from loguru import logger
 
 from validators import validate_secret, validate_sender
@@ -8,20 +10,42 @@ from validators import validate_secret, validate_sender
 validators = [validate_secret, validate_sender]
 app = FastAPI()
 
-LOG_FILEPATH = os.environ.get('LOG_FILEPATH') or './webhook.log'
-print(LOG_FILEPATH)
+LOG_FILEPATH = environ.get('LOG_FILEPATH') or './webhook.log'
 logger.add(LOG_FILEPATH,
            format='[{level}]\t{name}:{function}:{line}\t{message}')
+
+GIT_REPOPATH = environ.get('GIT_REPOPATH') or '/tmp/airflow-jobs-repo'
+GIT_REPO_URL = environ.get(
+    'GIT_REPO_URL', ) or 'https://github.com/fivestarsky/airflow-jobs'
+AIRFLOW_DAGS_PATH = environ.get(
+    'LOG_FILEPATH', ) or '/mnt/nfs4_share/airflow-pvs/dags'
 
 
 @app.post("/")
 async def read_root_hook(req: Request):
     for validator in validators:
-        is_valid = await validator(req)
-        if not is_valid:
+        #  is_valid = await validator(req)
+        #  if not is_valid:
+        if not await validator(req):
             logger.error(f'Failed to validate request: {validator.__name__}')
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
             return
-    #  req_obj = await req.json()
 
-    return {"Hello": "World"}
+    repo = None
+    if path.exists(GIT_REPOPATH):
+        repo = Repo(GIT_REPOPATH)
+        logger.info(f'{GIT_REPOPATH} already there')
+    else:
+        repo = Repo.clone_from(GIT_REPO_URL, GIT_REPOPATH)
+
+    origin = repo.remote('origin')
+    origin.pull()
+    # TODO shutil doesn't provide 'overwrite' features for now
+    # Delete the destination and copy dags to it
+    shutil.rmtree(AIRFLOW_DAGS_PATH)
+    shutil.copytree(
+        f'{GIT_REPOPATH}/dags',
+        AIRFLOW_DAGS_PATH,
+        #  dirs_exist_ok=True,
+    )
+    return ""
